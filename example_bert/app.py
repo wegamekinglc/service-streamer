1e56a1ff714c1b27e0f4a28f89d6869dc42b1fea
wegamekinglc <scrappedprince.li@gmail.com>
2019-08-10 14:17:25 +0800

    try to fix

--- example_bert/app.py
+++ example_bert/app.py
@@ -1,10 +1,12 @@
-from gevent import monkey; monkey.patch_all()
+import multiprocessing as mp
 from flask import Flask, request, jsonify
 from bert_serving.client import ConcurrentBertClient
 from service_streamer import RedisStreamer
 
 
 app = Flask(__name__)
+model = None
+streamer = None
 
 
 class BertModel:
@@ -29,10 +31,12 @@ def stream_predict():
     outputs = streamer.predict(inputs)
     return jsonify(list(outputs[0].astype(float)))
 
-model = BertModel()
 
 if __name__ == '__main__':
-    pass
+    mp.freeze_support()
+    mp.set_start_method("spawn", force=True)
+    model = Model()
+    streamer = ThreadedStreamer(model.predict, batch_size=64, max_latency=0.1)
     
     # app.run(host="0.0.0.0", port=5000, debug=False)
 
