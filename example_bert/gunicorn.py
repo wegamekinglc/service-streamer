7aed30cae7659c2c33f8911d87beec4c7be79097
wegamekinglc <scrappedprince.li@gmail.com>
2019-08-10 14:11:26 +0800

    change back

--- example_bert/gunicorn.py
+++ example_bert/gunicorn.py
@@ -1,8 +1,9 @@
 from gevent import monkey; monkey.patch_all()
 
-from service_streamer import RedisStreamer
-import app
-app.streamer = RedisStreamer()
+def post_fork(server, worker):
+    from service_streamer import RedisStreamer
+    import app
+    app.streamer = RedisStreamer()
 
 bind = '0.0.0.0:5000'
 workers = 4
