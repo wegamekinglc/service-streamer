3bf72dbd8ecc9b9d3fc0a336ea35a83fc0c4911c
wegamekinglc <scrappedprince.li@gmail.com>
2019-08-10 16:13:02 +0800

    fixed typo

--- example_bert/app.py
+++ example_bert/app.py
@@ -39,6 +39,6 @@ def stream_predict():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False, Threaded=False, processes=4)
+    app.run(host="0.0.0.0", port=5000, debug=False, threaded=False, processes=4)
     #from gevent.pywsgi import WSGIServer
     # server = WSGIServer(("0.0.0.0", 5000), app).serve_forever()
