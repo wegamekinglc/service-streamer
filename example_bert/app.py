245ab7a26fd03480f28022e2e4f0e085efb39f13
wegamekinglc <scrappedprince.li@gmail.com>
2019-08-10 16:37:44 +0800

    change

--- example_bert/app.py
+++ example_bert/app.py
@@ -1,14 +1,12 @@
 from gevent import monkey; monkey.patch_all()
-import multiprocessing as mp
-mp.freeze_support()
-mp.set_start_method("spawn", force=True)
-
 import numpy as np
 from flask import Flask, request, jsonify
 from bert_serving.client import ConcurrentBertClient
 from service_streamer import ThreadedStreamer
 
 app = Flask(__name__)
+model = None
+streamer = None
 
 
 class BertModel:
@@ -20,10 +18,6 @@ class BertModel:
         # batch_outputs = self.bc.encode(batch)
         # return batch_outputs
         return np.array([[1.0, 0.0]])
-
-
-model = BertModel()
-streamer = ThreadedStreamer(model.predict, batch_size=256, max_latency=0.1)
 
 
 @app.route("/naive", methods=["POST"])
@@ -42,5 +36,13 @@ def stream_predict():
 
 if __name__ == '__main__':
     # app.run(host="0.0.0.0", port=5000, debug=False)
+    import multiprocessing as mp
+    mp.freeze_support()
+    mp.set_start_method("spawn", force=True)
+
+    model = BertModel()
+    streamer = ThreadedStreamer(model.predict, batch_size=256, max_latency=0.1)
+
+
     from gevent.pywsgi import WSGIServer
     server = WSGIServer(("0.0.0.0", 5000), app).serve_forever()
